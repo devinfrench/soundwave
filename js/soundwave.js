5b0f450447a6fe17b77ce1084f8c870168775375
Devin <devinfrench@users.noreply.github.com>
2018-09-19 19:29:58 -0700

    Add tracks to queue

--- js/soundwave.js
+++ js/soundwave.js
@@ -1,3 +1,5 @@
+const queue = [];
+
 ready(() => {
     SC.initialize({
         client_id: "f06b9e9a56aa303794319cbeb8e12c7d"
@@ -12,15 +14,35 @@ ready(() => {
                 from: 60000
             }
         }).then((tracks) => {
-            console.log(tracks);
+            addTracksToQueue(tracks);
         });
     })
 });
+
+function addTracksToQueue(tracks) {
+    if (!tracks) return;
+
+    tracks.forEach((track, i) => {
+        queue.push(new SoundCloudTrack(
+            track["title"],
+            track["stream_url"],
+            track["artwork_url"]
+        ));
+    });
+}
 
 function ready(fn) {
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
         fn();
     } else {
         document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+class SoundCloudTrack {
+    constructor(title, stream, artwork) {
+        this.title = title;
+        this.stream = stream;
+        this.artwork = artwork;
     }
 }
